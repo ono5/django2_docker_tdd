1f7c9153332308ff3bb8b7605cdc1b46c33c68a4
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-02-28 22:38:20 +0900

    Ensuring We Have a Regression Test

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -17,7 +17,6 @@ class NewVisitorTest(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         cls.host = socket.gethostbyname(socket.gethostname())
-        print(cls.host)
         super(NewVisitorTest, cls).setUpClass()
 
     def setUp(self):
@@ -42,7 +41,7 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self. browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
         self.browser.get_screenshot_as_file('test.png')
 
@@ -78,6 +77,34 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
+        self.browser.quit()
+        self.browser = webdriver.Chrome
+
+        # Another Person access into Site
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        assert 'Buy peacock feathers' not in page_text
+        assert 'make a fly' not in page_text
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        assert francis_list_url not in edith_list_url
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        assert 'Buy peacock feathers' not in page_text
+        assert 'Buy milk' in page_text
