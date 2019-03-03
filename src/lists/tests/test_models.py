bc20e42f1d090409825a11547c18c4bd90b20b91
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-03-03 18:47:43 +0900

    Split out unit tests into tow files

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -5,17 +5,6 @@ import pytest
 from lists.models import Item, List
 
 
-class HomePageTest(TestCase):
-
-    def test_user_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        assert Item.objects.count() == 0
-
-
 class ListAndItemModelsTest(TestCase):
 
     @pytest.mark.django_db
