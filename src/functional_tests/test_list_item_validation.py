783d76316da8dbea79f073c4265f54ca7a6fc6d9
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-03-07 22:30:02 +0900

    Update functional test

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -1,10 +1,13 @@
 from selenium.webdriver.common.keys import Keys
-
+import pytest
 
 from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
@@ -45,6 +48,7 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
+    @pytest.mark.skip
     def test_cannot_add_duplicate_items(self):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys('Buy wellies')
@@ -58,4 +62,25 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
             "You've already got this in your list"
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a list and causes a validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message disappers
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
